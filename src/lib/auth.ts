f83039f1e6c6d76c2a8fe6650930a60967b2d0dd
Lars Devans <l.beijaard1@student.avans.nl>
2025-05-08 01:03:12 +0200

    Add user registration
    This includes storing the user credentials on Firebase

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { app } from "@/lib/firebase";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  UserCredential
+} from "firebase/auth";
+
+const auth = getAuth(app);
+
+export async function signUp(
+  email: string,
+  password: string
+): Promise<UserCredential | null> {
+  try {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error: any) {
+    console.error("Firebase foutmelding, details in console:", error.code);
+    return null;
+  }
+}
