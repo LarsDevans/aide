0931338b778f1eb23d46eb68827c504e81fbd078
Lars Devans <l.beijaard1@student.avans.nl>
2025-05-10 17:35:54 +0200

    Merged UI components to single type file (#9)

---
+++ src/types/ui.ts
@@ -0,0 +1,16 @@
+import { ChangeEvent, FormEvent } from "react";
+
+export interface Button {
+  disabled: boolean;
+  label: string;
+  type: "submit";
+  onClick?: (e: FormEvent<HTMLButtonElement>) => void;
+}
+
+export interface Input {
+  name: string;
+  placeholder: string;
+  type: "email" | "password";
+  value?: string | undefined;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
