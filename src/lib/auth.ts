bb276b883813e64fb8dab1632d132efaaf04eb5d
Lars Devans <l.beijaard1@student.avans.nl>
2025-05-11 12:25:50 +0200

    Add login to session storage (#12)
    * Add login to session storage
    
    * Hide type any from linter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
 import { app } from "@/lib/firebase";
 import {
+  browserSessionPersistence,
   createUserWithEmailAndPassword,
   getAuth,
   onAuthStateChanged,
+  setPersistence,
   signInWithEmailAndPassword,
   User,
   UserCredential
@@ -39,10 +41,12 @@ export async function signIn(
   email: string,
   password: string
 ): Promise<UserCredential | null> {
-  try {
-    return await signInWithEmailAndPassword(auth, email, password);
-  } catch (error: any) { // eslint-disable-line
-    console.error("Firebase foutmelding, details in console:", error.code);
-    return null;
-  }
+  return await setPersistence(auth, browserSessionPersistence)
+    .then(async () => {
+      return await signInWithEmailAndPassword(auth, email, password);
+    })
+    .catch((error: any) => { // eslint-disable-line
+      console.error("Firebase foutmelding, details in console:", error.code);
+      return null;
+    });
 }
