68a1265966b50aa97e03620b3f983d54b4b62f28
Lars Devans <l.beijaard1@student.avans.nl>
2025-05-15 10:59:57 +0200

    Shorten error message

--- src/lib/validation/silo.ts
+++ src/lib/validation/silo.ts
@@ -3,14 +3,14 @@ import { z } from "zod";
 export const createSchema = z.object({
   name: z
     .string()
-    .min(3, { message: "De naam moet minimaal 3 karakters lang zijn"}),
+    .min(3, { message: "De naam is te kort (min. 3)"}),
   description: z
     .string(),
 });
 export const updateSchema = z.object({
   name: z
     .string()
-    .min(3, { message: "De naam moet minimaal 3 karakters lang zijn"}),
+    .min(3, { message: "De naam is te kort (min. 3)"}),
   description: z
     .string(),
 });
