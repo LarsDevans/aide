6e921c503acced58d9f5ef0d96b35eb77aab63a3
Lars Devans <l.beijaard1@student.avans.nl>
2025-05-18 13:04:10 +0200

    Refactor auth routing files

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,5 @@
-import AuthLayout from "@/app/auth/layout"
 import LoginForm from "@/components/auth/LoginForm"
 
 export default function LoginPage() {
-  return (
-    <AuthLayout>
-      <LoginForm />
-    </AuthLayout>
-  )
+  return <LoginForm />
 }
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,10 +1,5 @@
-import AuthLayout from "@/app/auth/layout"
 import RegisterForm from "@/components/auth/RegisterForm"
 
 export default function RegisterPage() {
-  return (
-    <AuthLayout>
-      <RegisterForm />
-    </AuthLayout>
-  )
+  return <RegisterForm />
 }
